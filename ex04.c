276f4ff10420b0351180bed91d1f821d665b9626
Chihiro1231 <83006053+Chihiro1231@users.noreply.github.com>
2022-07-14 11:08:01 +0900

    HoT: Initial revision

---
+++ ex04.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  int i, n, cnt1=0, cnt2=0;
+
+  srand((unsigned int)time(NULL));
+  
+  printf("Tossing a coin...\n");
+  
+  for(i=0; i<3; i++) {
+    printf("Round %d: ", i+1);
+    n = rand() ;
+    if(n%2==0) {
+      printf("Heads\n");
+      cnt1++;
+    }else{
+      printf("Tails\n");
+      cnt2++;
+    }
+  }
+  
+  printf("Heads: %d, Tails: %d\n", cnt1, cnt2);
+  
+  return 0;
+}
